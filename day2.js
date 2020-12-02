2819d008b8997cddd12e3479d9840e88757d51c1
David Dudley <DavidDudley@rentalcars.com>
2020-12-02 17:58:17 +0000

    AOC - Day Two - Part One

--- day2.js
+++ day2.js
@@ -1,26 +1,27 @@
-const { countReset } = require('console');
-const { groupPatternsByBaseDirectory } = require('fast-glob/out/managers/tasks');
 const fs = require('fs');
 
 const lines = fs.readFileSync('index.txt', {encoding: 'utf-8'}).split('\n').filter(x => x);
 
 let validPasswords = 0;
 
-lines.forEach(line => {
-    console.log(lines)
-    const {groups} = /^(?<firstNum>\d+)-(?<secondNum>\d+) (?<char>.): (?<password>.*)$/.exec(line);
 
+//sort index into objects and label
+lines.forEach(line => {
+    const {groups} = /^(?<firstNum>\d+)-(?<secondNum>\d+) (?<char>.): (?<password>.*)$/.exec(line);
+   // console.log(groups);
     const counter = {};
 
-    console.log(groups);
+   //Step One
+   // console.log(groups);
     [...groups.password].forEach(char => {
         if(!counter[char]){
             counter[char] = 0;
-        }
+        };
         counter[char]++;
     })
     if(counter[groups.char] >= groups.firstNum && counter[groups.char] <= groups.secondNum){
         validPasswords++;
     }
-    console.log(validPasswords)
-})
+}
+)
+console.log("List of Valid Passwords = " +  validPasswords)
